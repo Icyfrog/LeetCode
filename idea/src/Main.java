c9c24717a8ebd8d786c7c387b1de5d81ea95bffc
IcyFrog <sjtu-yqc@sjtu.edu.cn>
2019-10-16 21:03:15 +0800

    two sum over

--- idea/src/Main.java
+++ idea/src/Main.java
@@ -1,6 +1,38 @@
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        /*HashMap<Integer,Integer> map = new HashMap();
+
+        map.put(1,100);
+
+        map.put(2,200);
+
+        map.put(1,300);
+
+        System.out.println(map.containsValue(100));
+*/
+        int[] nums={5,5,3,3,5};
+        int target = 6;
+        int[] res = new int[2];
+        int size = nums.length;
+        HashMap<Integer, Integer> hash_map = new HashMap();
+        for (int i = 0; i < size; i++) {
+            hash_map.put(nums[i], i);
+        }
+        for (int i = 0; i < size; i++) {
+            int b = target - nums[i];
+            if (hash_map.containsKey(b) && i != hash_map.get(b)) {
+                res[0] = i;
+                res[1] = hash_map.get(b);
+                break;
+            }
+        }
+
+        System.out.println(res[0]);
+        System.out.println(res[1]);
+
     }
 }
---
+++ 两数之和 2019-10-15/README.md
@@ -0,0 +1,8 @@
+## [题目](https://leetcode-cn.com/problems/two-sum/?utm_source=LCUS&utm_medium=ip_redirect_q_uns&utm_campaign=transfer2china)
+## 
+
+1. 直接暴力解法，两重循环，效率一般
+2. [链接](https://blog.csdn.net/SoulOH/article/details/81017361) 用Hash表**根据key来查value的时间复杂度为O1**，来提升代码效率.
+3. 同时注意到，找到3、5和找到5、3效果是一样的，所以完全可以将数组哈希和查询遍历放在同一个循环内，又节省了时间。
+4. 如果数组中有重复的，会不会hash表中会不会覆盖，或是新增一个键值对呢？其实这个题目根本不用考虑hash表是否会重复！因为两个下标，一个是从循环计数器i中拿到的，一个是从哈希表的key中拿到的。
+5. [参见](https://www.cnblogs.com/aflyun/p/10733029.html)
